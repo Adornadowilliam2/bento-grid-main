f0c04d2a5397a73f6723a93ab553035c55e0dbe1
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-11-02 19:19:49 +0800

    adjusting issue i learn from front end mentor

--- src/App.jsx
+++ src/App.jsx
@@ -12,49 +12,49 @@ import growfollowergraph from "./assets/images/illustration-grow-followers.webp"
 function App() {
   return (
     <>
-      <div className="grid-container ">
-        <div className="createpost container">
+      <main className="grid-container ">
+        <section className="createpost container">
           <h1>
             Create and schedule content <span className="purple">quicker.</span>
           </h1>
           <img src={createpostbtn} alt="create-post-btn" />
-        </div>
-        <div className="social container">
-          <h1 className="white">
+        </section>
+        <section className="social container">
+          <h2 className="white">
             Social Media <span className="gold">10x</span> Faster with AI
-          </h1>
+          </h2>
           <img src={fivestar} alt="5-star" />
           <p className="white">Over 4,000 5-star reviews</p>
-        </div>
-        <div className="schedule container">
-          <h1>Schedule to social media.</h1>
+        </section>
+        <section className="schedule container">
+          <h2>Schedule to social media.</h2>
           <img src={graphpost} alt="illustraion graph" />
           <p>
             optimize post timings to publish content at the perfect time for
             your audience.
           </p>
-        </div>
-        <div className="content container">
-          <h1>Write your content using AI.</h1>
+        </section>
+        <section className="content container">
+          <h2>Write your content using AI.</h2>
           <img src={writecontent} alt="ai-content" />
-        </div>
-        <div className="manage container">
+        </section>
+        <section className="manage container">
           <img src={account} alt="manage multiple account" />
-          <h1>Manage multiple accounts and platforms.</h1>
-        </div>
-        <div className="maintain container">
-          <h1>Maintain a consistent posting schedule.</h1>
+          <h2>Manage multiple accounts and platforms.</h2>
+        </section>
+        <section className="maintain container">
+          <h2>Maintain a consistent posting schedule.</h2>
           <img src={calendar} alt="calendar" />
-        </div>
-        <div className="audience container">
-          <h1>{">"}56%</h1>
+        </section>
+        <section className="audience container">
+          <h2>{">"}56%</h2>
           <p>faster audience growth</p>
           <img src={icons} alt="icons" />
-        </div>
-        <div className="grow container">
+        </section>
+        <section className="grow container">
           <img src={growfollowergraph} alt="graph" />
-          <h1 className="white">Grow followers with non-stop content.</h1>
-        </div>
+          <h2 className="white">Grow followers with non-stop content.</h2>
+        </section>
         <div class="attribution">
           Challenge by{" "}
           <a href="https://www.frontendmentor.io?ref=challenge">
@@ -64,7 +64,7 @@ function App() {
           <a href="https://github.com/Adornadowilliam2">William Adornado Jr.</a>
           .
         </div>
-      </div>
+      </main>
     </>
   );
 }
